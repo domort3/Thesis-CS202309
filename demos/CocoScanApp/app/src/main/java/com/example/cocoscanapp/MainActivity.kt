43e5ec6bc3be84fd94caec3a2c231f1d7e729501
buff-gorila <domingoortegaiii@gmail.com>
2024-07-01 15:25:47 +0800

    issues on binding reference

--- demos/CocoScanApp/app/src/main/java/com/example/cocoscanapp/MainActivity.kt
+++ demos/CocoScanApp/app/src/main/java/com/example/cocoscanapp/MainActivity.kt
@@ -7,6 +7,8 @@ import android.graphics.Bitmap
 import android.os.Build
 import android.os.Bundle
 import android.provider.MediaStore
+import android.view.LayoutInflater
+import android.view.ViewGroup
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
@@ -24,6 +26,8 @@ class MainActivity : AppCompatActivity() {
     private lateinit var bottomNavigationView: BottomNavigationView
     private lateinit var fabCenter: FloatingActionButton
     private val REQUEST_IMAGE_CAPTURE = 100
+
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
